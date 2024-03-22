57ac7271975357e785cde842a5c6be9f25f10a77
sktrinh12 <sktrinh12@gmail.com>
2024-03-22 14:45:30 -0400

    removed index.tsx & added styles css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Applayout from "./App.tsx";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
